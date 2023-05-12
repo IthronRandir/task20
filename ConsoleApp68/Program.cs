779287ded6d478d6d0da7880ec3aa5a3522ad590
IthronRandir <frtopo@yandex.by>
2023-05-12 17:24:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp68/Program.cs
@@ -0,0 +1,28 @@
+﻿class Programm
+{
+    static int CountLessNSimple(int n)
+    {
+        int Count = 1;
+        bool b = true;
+        for (int LessN = 2; LessN < n; LessN++)
+        {
+            for (int i = 2; i < LessN; i++)
+            {
+                if (LessN % i == 0)
+                {
+                    b = true;
+                    break;
+                }
+                else b = false;
+            }
+            if (b == false) Count++;
+        }
+        return Count;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = 10;
+        Console.WriteLine(CountLessNSimple(n));
+    }
+}
